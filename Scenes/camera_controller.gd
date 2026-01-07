640170d2a8e32d0734823d3d90b6e7682331e5a3
ZaneAhmed <zane.mahmoud212@gmail.com>
2026-01-07 23:44:02 +0100

    Dev console very basic

--- Scenes/camera_controller.gd
+++ Scenes/camera_controller.gd
@@ -15,11 +15,11 @@ var is_dragging := false
 
 
 func _process(delta: float) -> void:
-	if GameState.decision_tree_open: return
+	if GameState.decision_tree_open or Console.is_visible(): return
 	_handle_keyboard_movement(delta)
 
 func _input(event: InputEvent) -> void:
-	if GameState.decision_tree_open: return
+	if GameState.decision_tree_open or Console.is_visible(): return
 	
 	if event.is_action_pressed("pause_game"):
 		MainClock.toggle_pause()
